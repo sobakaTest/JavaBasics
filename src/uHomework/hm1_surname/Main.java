1b8a815ae6058d86fe9c3f3c8a7afc74c6f7b7f8
aleksandrs.grisanovs <aleksandrs.grisanovs@accenture.com>
2020-02-10 10:14:04 +0200

    test push

---
+++ src/uHomework/hm1_surname/Main.java
@@ -0,0 +1,7 @@
+package uHomework.hm1_surname;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/uHomework/hm3_surname/Main.java
@@ -0,0 +1,7 @@
+package uHomework.hm3_surname;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
